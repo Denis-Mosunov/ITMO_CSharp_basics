e6f55ef250f47ebad49ffc3e382277d5cec3474d
Denis-Mosunov <dnsmosunov@gmail.com>
2025-04-24 15:30:04 +0300

    Create Exm_01.cs

---
+++ Лабораторная работа 1. Создание программы/Упражнение 5. Расчет площади треугольника/Exm_01.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Xml.Linq;
+
+namespace Exm_01
+{
+    //-- Программа расчёта площади равностороннего треугольника, периметр которого известен
+
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Please enter Perimetr value");;
+            double rPerimetr = Double.Parse( Console.ReadLine() );
+            Console.WriteLine("Please enter Storona value");
+
+            double rStorona = Double.Parse( Console.ReadLine() );
+
+            double rS = Math.Sqrt(rPerimetr * (rPerimetr - rStorona) * (rPerimetr - rStorona) * (rPerimetr - rStorona));
+
+            Console.WriteLine("\tSTORONA \t\tPLOSHAD");
+            Console.WriteLine("\t  {0:F2} \t\t {1:F2}", rStorona, rS);
+        }
+    }
+}
